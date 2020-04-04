6d238e708669ceea7a9c7fc1014b5955c61bb68f
oudun <oudun@mail.ru>
2020-04-05 02:13:04 +0300

    Secret key added

--- manage.py
+++ manage.py
@@ -1,6 +1,5 @@
 import sys
 import os
-from django.conf import settings
 from django.core.management import execute_from_command_line
 from django.http import HttpResponse
 from django.urls import path
@@ -11,8 +10,6 @@ def hello(request):
 urlpatterns = [
 path('', hello)
 ]
-
-SECRET_KEY = 'key'
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "type_one.settings")
 execute_from_command_line(sys.argv)
