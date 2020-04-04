0c364da16343b18f2f19c93ca16b17578827af95
oudun <oudun@mail.ru>
2020-04-05 01:40:38 +0300

    just commit

--- manage.py
+++ manage.py
@@ -1,10 +1,9 @@
 import sys
+import os
 from django.conf import settings
 from django.core.management import execute_from_command_line
 from django.http import HttpResponse
 from django.urls import path
-
-settings.configure(ROOT_URLCONF=__name__, ALLOWED_HOSTS='*')
 
 def hello(request):
     return HttpResponse('Hello World')
@@ -13,4 +12,7 @@ urlpatterns = [
 path('', hello)
 ]
 
+SECRET_KEY = 'key'
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "type_one.settings")
 execute_from_command_line(sys.argv)
