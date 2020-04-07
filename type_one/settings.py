1a18694ca39b847cc610f2d1eae80c48936f5d0d
oudun <oudun@mail.ru>
2020-04-08 02:10:47 +0300

    login form

--- type_one/settings.py
+++ type_one/settings.py
@@ -1,11 +1,47 @@
+import environ
+
+env = environ.Env()
+
 SECRET_KEY = 'key'
+
+DEBUG = True
+
+SITE_ID = 1
+
+DATABASES = {
+    "default": env.db()
+    }
 
 INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.auth',
+    'django.contrib.messages',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'type_one.core'
     ]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.request',
+            ],
+        },
+    },
+]
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+)
 
 AUTH_USER_MODEL = "core.User"
 
 ROOT_URLCONF = "type_one.urls"
+
--- type_one/urls.py
+++ type_one/urls.py
@@ -1,2 +1,6 @@
+from django.conf.urls import url
+from django.conf.urls import include
+
 urlpatterns = [
+    url('accounts/', include('allauth.urls')),
 ]
