a33dcfd0d4ade89bf2a32f5bbb0ad723d3582a5e
oudun <oudun@mail.ru>
2020-04-07 22:45:21 +0300

    gitignore

--- manage.py
+++ manage.py
@@ -1,11 +1,6 @@
 import sys
 import os
 from django.core.management import execute_from_command_line
-from django.http import HttpResponse
-
-def hello(request):
-    return HttpResponse('Hello World')
-
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "type_one.settings")
 execute_from_command_line(sys.argv)
