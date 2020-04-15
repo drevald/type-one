1998435ecd2aa2b4f76d44bb7e98c2c502663d25
oudun <oudun@mail.ru>
2020-04-16 00:32:37 +0300

    hello url added

--- type_one/urls.py
+++ type_one/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from django.conf.urls import include
+from django.urls import path
 
 urlpatterns = [
     url('accounts/', include('allauth.urls')),
+    path('', include('type_one.core.urls'))
 ]
