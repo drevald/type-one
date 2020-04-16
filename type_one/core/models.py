9c15c1f7b1c8bff387c720feb34e2b6ff9f8ee6c
oudun <oudun@mail.ru>
2020-04-16 20:13:55 +0300

    model updated

--- type_one/core/models.py
+++ type_one/core/models.py
@@ -1,5 +1,19 @@
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 class User(AbstractUser):
     pass
 
+class Unit(models.Model):
+    code = models.CharField(max_length=200)
+
+class Insulin(models.Model):
+    code = models.CharField(max_length=100)
+
+class InsulinShot(models.Model):
+    amount_units = models.FloatField
+    insulin = models.ForeignKey(
+        Insulin, 
+        on_delete=models.CASCADE, 
+        default=1)
+    time = models.TimeField
