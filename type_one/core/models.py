a6e8da7052caa679fcae18f73ff5657e0022221e
oudun <oudun@mail.ru>
2020-04-24 00:31:27 +0300

    setup script added

--- type_one/core/models.py
+++ type_one/core/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 class Insulin(models.Model):
     code = models.CharField(max_length=100)
+    def __str__(self):
+        return self.code
 
 class SugarLevelUnit(models.Model):
     code = models.CharField(max_length = 100)
@@ -10,9 +12,9 @@ class SugarLevelUnit(models.Model):
         return self.code
 
 class User(AbstractUser):
-    sugar_level_unit = models.ForeignKey(SugarLevelUnit, on_delete = models.DO_NOTHING)
-    long_acting_insulin = models.ForeignKey(Insulin, related_name='long', on_delete = models.DO_NOTHING)
-    rapid_acting_insulin = models.ForeignKey(Insulin, related_name='rapid', on_delete = models.DO_NOTHING)
+    sugar_level_unit = models.ForeignKey(SugarLevelUnit, on_delete = models.SET_NULL, null=True)
+    long_acting_insulin = models.ForeignKey(Insulin, related_name='long', on_delete = models.SET_NULL, null=True)
+    rapid_acting_insulin = models.ForeignKey(Insulin, related_name='rapid', on_delete = models.SET_NULL, null=True)
 
 class InsulinShot(models.Model):
     amount_units = models.FloatField()
@@ -26,33 +28,35 @@ class Activity(models.Model):
     code = models.CharField(max_length = 100)
     def __str__(self):
         return self.code
+    class Meta():
+        verbose_name_plural = "Activities"
 
 class ActivityPeriod(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     activity = models.ForeignKey(Activity, on_delete = models.DO_NOTHING)
     startTime = models.TimeField()
     endTime = models.TimeField()
 
 class Unit(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     code = models.CharField(max_length = 255)
     def __str__(self):
         return self.code
 
 class Ingredient(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     code = models.CharField(max_length = 255)
-    name = models.CharField(max_length = 255)
-    glycemicIndex = models.IntegerField()
     breadUnitsPer100g = models.FloatField()
+    glycemicIndex = models.IntegerField()
     fatPer100g = models.IntegerField()
     carbohydratePer100g = models.IntegerField()
     proteinPer100g = models.IntegerField()
     energyKkalPer100g = models.IntegerField()
     def __str__(self):
-        return self.name
+        return self.code
 
 class IngredientUnit(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     unit = models.ForeignKey(Unit, on_delete = models.DO_NOTHING)
     ingredient = models.ForeignKey(Ingredient, on_delete = models.DO_NOTHING)
     gramsInUnit = models.FloatField()
@@ -60,17 +64,17 @@ class IngredientUnit(models.Model):
         return self.ingredient.code + "_" + self.unit.code
 
 class Meal(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     name = models.CharField(max_length = 255)
 
 class MealIngredient(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     meal = models.ForeignKey(Meal, on_delete = models.DO_NOTHING)
     indredientUnitId = models.ForeignKey(IngredientUnit, on_delete = models.DO_NOTHING)
     ingredientWeightGrams = models.IntegerField()
 
 class Record(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     time = models.TimeField()
     sugarLevel = models.ForeignKey(SugarLevel, on_delete = models.DO_NOTHING)
     insulinShot = models.ForeignKey(InsulinShot, on_delete = models.DO_NOTHING)
---
+++ type_one/setup.sql
@@ -0,0 +1,57 @@
+insert into core_ingredient values (0, 'sugar', 8, 70, 0, 100, 0, 400, null));
+insert into core_ingredient values (1, 'milk', 0.5,30, 3, 4, 2, 60, null);
+insert into core_ingredient values (2, 'ray_bread', 4, 50, 1, 44, 4, 210, null);
+insert into core_ingredient values (3, 'wheat_bread', 5, 80, 1, 49, 1, 238, null);
+insert into core_ingredient values (4, 'potato_boiled', 1.5, 65, 0, 18, 2, 79, null);
+insert into core_ingredient values (5, 'potato_fried', 2.8, 95, 9, 24, 1, 192, null);
+insert into core_ingredient values (6, 'spaghetti', 2, 55, 1, 27, 6, 140, null);
+insert into core_ingredient values (7, 'buckwheat', 1.5, 40, 2, 17, 4, 100, null);
+insert into core_ingredient values (8, 'rice', 2, 80, 0, 25, 2, 113, null);
+insert into core_ingredient values (9, 'butter', 0.1, 51, 83, 1, 1, 748, null); //GI ?
+insert into core_ingredient values (10, 'cheese', 0, 15, 14, 3, 11, 183, null);
+insert into core_ingredient values (11, 'sausage', 0, 28, 28, 2, 10, 300, null);
+insert into core_ingredient values (12, 'burger', 0, 40, 20, 5, 16, 266, null);
+insert into core_ingredient values (13, 'pork_boiled', 0, 0, 30, 0, 23, 376, null);
+insert into core_ingredient values (14, 'pork_fried', 0, 0, 50, 0, 11, 489, null);
+insert into core_ingredient values (15, 'beef_boiled', 0, 0, 17, 0, 26, 252, null);
+insert into core_ingredient values (16, 'beef_fried', 0, 0, 28, 0, 33, 384, null);
+insert into core_ingredient values (17, 'chicken_boiled', 0, 0, 7, 0, 25, 170, null);
+insert into core_ingredient values (18, 'chicken_fried', 0, 0, 12, 0, 26, 210, null);
+insert into core_ingredient values (19, 'egg', 0.1, 0, 12, 1, 13, 160, null);
+insert into core_ingredient values (20, 'omelette', 0.2, 50, 20, 3, 14, 250, null);
+
+insert into core_unit values (0, 'gramm', null);
+insert into core_unit values (1, 'ml', null);
+insert into core_unit values (2, 'piece', null);
+insert into core_unit values (3, 'glass', null);
+insert into core_unit values (4, 'teaspoon', null);
+insert into core_unit values (5, 'spoon', null);
+insert into core_unit values (6, 'plate', null);
+
+insert into core_ingredientunit values (DEFAULT, 1, 0, 0, null));
+insert into core_ingredientunit values (DEFAULT, 1, 1, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 2, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 3, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 4, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 5, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 6, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 7, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 8, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 9, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 10, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 11, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 12, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 13, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 14, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 15, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 16, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 17, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 18, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 19, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1, 20, 0, null);
+insert into core_ingredientunit values (DEFAULT, 1.04, 1, 1, null);
+insert into core_ingredientunit values (DEFAULT, 100, 2, 2, null);
+insert into core_ingredientunit values (DEFAULT, 35, 3, 2, null);
+insert into core_ingredientunit values (DEFAULT, 25, 4, 2, null);
+
+
