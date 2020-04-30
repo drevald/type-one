815463d1df11a64496c60856a87fc950f4584cd8
oudun <oudun@mail.ru>
2020-04-30 12:14:03 +0300

    the purge

--- manage.py
+++
@@ -1,6 +0,0 @@
-import sys
-import os
-from django.core.management import execute_from_command_line
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "type_one.settings")
-execute_from_command_line(sys.argv)
