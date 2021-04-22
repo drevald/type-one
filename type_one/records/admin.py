f4772e1c0408c855f4096fedef9a1a260e797779
Oudun <oudun@mail.ru>
2021-04-23 00:47:46 +0300

    Admin UI fixed

--- type_one/records/admin.py
+++ type_one/records/admin.py
@@ -1,6 +1,15 @@
 from django.contrib import admin
 from .models import GlucoseUnit, Insulin, User
+from django.contrib.auth.admin import UserAdmin
+
+class TypeOneAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('glucose_level_unit','long_acting_insulin','rapid_acting_insulin',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('glucose_level_unit','long_acting_insulin','rapid_acting_insulin',)}),
+    )
 
 admin.site.register(GlucoseUnit)
 admin.site.register(Insulin)
-admin.site.register(User)
+admin.site.register(User, TypeOneAdmin)
