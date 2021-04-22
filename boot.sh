29c8b4e021d8f15e5285931cb122a53061c10eff
Oudun <oudun@mail.ru>
2021-04-22 18:09:15 +0300

    message compilation narrowed to 2 locales

--- boot.sh
+++ boot.sh
@@ -1,3 +1,5 @@
-python manage.py compilemessages
+python manage.py compilemessages --locale ru_RU
+python manage.py compilemessages --locale en_EN
 python manage.py collectstatic --noinput
+python manage.py migrate
 exec gunicorn type_one.wsgi:application --bind 0.0.0.0:$PORT --timeout 120 --workers=3 --threads=3 --worker-connections=1000 --log-file -
