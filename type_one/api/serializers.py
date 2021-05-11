5eafca3eb4ff0c749ecffd7aad0238cc27ec4ab5
Oudun <oudun@mail.ru>
2021-05-12 02:32:03 +0300

    Cosmetic changes

--- type_one/api/serializers.py
+++ type_one/api/serializers.py
@@ -11,6 +11,16 @@ class PhotoSerializer(serializers.Serializer):
     def update(self, instance, validated_data):
         instance.data = validated_data.get('thumb', instance.thumb)
 
+class PhotoFullSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    data = serializers.StringRelatedField()
+
+    def create(self, validated_data):
+        return Photo.objects.created(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.data = validated_data.get('data', instance.data)        
+
 class GlucoseUnitSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.StringRelatedField()
@@ -45,6 +55,25 @@ class RecordSerializer(serializers.Serializer):
     notes = serializers.StringRelatedField()
     glucose_level_unit = GlucoseUnitSerializer(required=True)
     photos = PhotoSerializer(many=True, read_only=True)
+
+    def create(self, validated_data):
+        return Record.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.time = validated_data.get('time', instance.time)
+        instance.save()
+        return instance
+
+class RecordFullSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    time = serializers.DateTimeField(required=True, format='%B %d %H:%M')
+    bread_units = serializers.FloatField()
+    glucose_level = serializers.FloatField(required=True)
+    insulin = InsulinSerializer(many=False, required=True)
+    insulin_amount = serializers.IntegerField()
+    notes = serializers.StringRelatedField()
+    glucose_level_unit = GlucoseUnitSerializer(required=True)
+    photos = PhotoFullSerializer(many=True, read_only=True)
 
     def create(self, validated_data):
         return Record.objects.create(**validated_data)
--- type_one/api/views.py
+++ type_one/api/views.py
@@ -2,11 +2,12 @@ from rest_framework import generics
 from type_one.records.models import Record
 from type_one.records.models import Insulin
 from type_one.api.serializers import RecordSerializer
+from type_one.api.serializers import RecordFullSerializer
 from type_one.api.serializers import InsulinSerializer
 
 class RecordDetails(generics.RetrieveUpdateDestroyAPIView):
     queryset = Record.objects.all()
-    serializer_class = RecordSerializer
+    serializer_class = RecordFullSerializer
 
 class RecordsList(generics.ListCreateAPIView):
     queryset = Record.objects.all()
